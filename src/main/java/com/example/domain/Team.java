a56f766689997baa675cb521228bcc03bf7588c0
市野　那由他 <67714705+nayuta-ichino@users.noreply.github.com>
2021-08-17 10:40:52 +0900

    TeamDomain作成

---
+++ src/main/java/com/example/domain/Team.java
@@ -0,0 +1,85 @@
+package com.example.domain;
+
+import java.util.Date;
+
+/**
+ * チームの情報を表すドメイン.
+ * 
+ * @author nayuta
+ */
+public class Team {
+	/** ID */
+	private Integer id;
+	/** リーグ名 */
+	private String leagueName;
+	/** チーム名 */
+	private String teamName;
+	/** 本拠地 */
+	private String headquarters;
+	/** 発足年 */
+	private Date inauguration;
+	/** 歴史 */
+	private String history;
+
+	// コンストラクタ
+	public Team() {
+		// 引数、処理なし
+	}
+
+	// getter,setter
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLeagueName() {
+		return leagueName;
+	}
+
+	public void setLeagueName(String leagueName) {
+		this.leagueName = leagueName;
+	}
+
+	public String getTeamName() {
+		return teamName;
+	}
+
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+	public String getHeadquarters() {
+		return headquarters;
+	}
+
+	public void setHeadquarters(String headquarters) {
+		this.headquarters = headquarters;
+	}
+
+	public Date getInauguration() {
+		return inauguration;
+	}
+
+	public void setInauguration(Date inauguration) {
+		this.inauguration = inauguration;
+	}
+
+	public String getHistory() {
+		return history;
+	}
+
+	public void setHistory(String history) {
+		this.history = history;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "Team [id=" + id + ", leagueName=" + leagueName + ", teamName=" + teamName + ", headquarters="
+				+ headquarters + ", inauguration=" + inauguration + ", history=" + history + "]";
+	}
+
+}
