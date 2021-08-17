5c19542d4990fd20e50fef70518664418bb5d9c1
市野　那由他 <67714705+nayuta-ichino@users.noreply.github.com>
2021-08-17 12:05:44 +0900

    TeamControllerを作成

---
+++ src/main/java/com/example/controller/TeamController.java
@@ -0,0 +1,50 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.Service.TeamService;
+import com.example.domain.Team;
+
+@Controller
+@RequestMapping("/exam1")
+public class TeamController {
+	@Autowired
+	private TeamService teamService;
+
+	/**
+	 * チーム一覧を表示.
+	 * 
+	 * @param model requestスコープ
+	 * @return トップページへ遷移
+	 */
+	@RequestMapping("")
+	public String index(Model model) {
+		List<Team> teamList = teamService.findAll();
+
+		model.addAttribute("teamList", teamList);
+
+		return "Exam1/teamsList";
+	}
+
+	/**
+	 * チームの詳細を表示.
+	 * 
+	 * @param model requestパラメーター
+	 * @param id ID
+	 * @return 詳細ページへ遷移
+	 */
+	@RequestMapping("/team-show")
+	public String teamShow(Model model, Integer id) {
+		Team team = teamService.load(id);
+
+		model.addAttribute("team", team);
+
+		return "Exam1/teamsDetail";
+	}
+
+}
